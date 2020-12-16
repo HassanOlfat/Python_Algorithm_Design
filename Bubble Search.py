c5b8c7ce3733bd5f04591ea7e10c8f7f7095f9e3
hassan olfat <hassan.olfat@LIVE.COM>
2020-12-16 22:57:25 +0330

    BSearch

---
+++ Bubble Search.py
@@ -0,0 +1,24 @@
+import math
+
+def BSearch(a,x,low,high):
+    if low<=high:
+        mid=math.floor((low+high)/2)  
+        if a[mid]==x :
+            print(a[mid])
+        if x<a[mid]:
+            BSearch(a,x,low,mid-1)
+        elif x>a[mid]:
+            BSearch(a,x,mid+1,high)
+        
+    if low>high:
+        return -1
+
+#################################################################
+arry=[5,9,10,20,35,50,60,70,75]
+x=60
+low=0
+high=len(arry) -1
+
+
+BSearch(arry,x,low,high)
+
--- Insertion Sort.py
+++ Insertion Sort.py
@@ -1,15 +1,18 @@
 A = [5, 2, 4, 6, 1 ,3]
-j=1
-for j in range(len(A))  :
-     key = A[j] 
-     A[j]=A[j-1]
-     i=j-1
-     while i>=0 and A[i]>key :
-        A[i+1]=A[i]
-        i=i-1
-     A[i+1]=key
 
-print(A)
+def InsertionSort(n):
+   j=1
+   for j in range(len(n))  :
+      key = n[j] 
+      n[j]=n[j-1]
+      i=j-1
+      while i>=0 and n[i]>key :
+         n[i+1]=n[i]
+         i=i-1
+      n[i+1]=key
+   return n
+
+print(InsertionSort(A))
   
     
  
